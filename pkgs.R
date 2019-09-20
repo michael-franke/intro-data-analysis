0ae832f3eed49fe774f4635c15e5dee9c7bb976f
MarauderPixie <tobias.anton.85@gmail.com>
2019-09-20 14:09:40 +0200

    "outsource" loaded packages & defaults

---
+++ pkgs.R
@@ -0,0 +1,9 @@
+## load packages
+library(dplyr)
+library(ggplot2)
+library(brms)
+library(ggsignif)
+
+## set defaults
+# hrbrthemes::import_public_sans()
+theme_set(hrbrthemes::theme_ipsum(plot_margin = margin(10, 10, 10, 10)))
