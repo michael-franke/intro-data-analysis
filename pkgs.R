d2e4c8457c06daebdc18aeb9c0134aa9398a5c29
MarauderPixie <tobias.anton.85@gmail.com>
2020-01-07 15:43:32 +0100

    declutter pkgs.R a bit

--- pkgs.R
+++ pkgs.R
@@ -1,21 +1,20 @@
 ## load packages
-suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(brms))
-suppressPackageStartupMessages(library(ggsignif))
-suppressPackageStartupMessages(library(gridExtra))
-suppressPackageStartupMessages(library(Rlab)) # for dbern (used in appendix; maybe solve differently?)
-suppressPackageStartupMessages(library(rcartocolor)) # color-blind friendly palettes: https://github.com/Nowosad/rcartocolor
-suppressPackageStartupMessages(library(extraDistr))  # create beta-binomial and bernoulli rvs
-suppressPackageStartupMessages(library(boot))  # bootstrapped (mainly: 95% CIs for the mean)
-# suppressPackageStartupMessages(library(rethinking))
-suppressPackageStartupMessages(library(naniar)) # turn numbers (e.g. -5,99,..) into NA (handling of missing values in data sets)
-suppressPackageStartupMessages(library(latex2exp)) # for TeX in plots
-suppressPackageStartupMessages(library(cowplot)) # to arrange plots
-suppressPackageStartupMessages(library(scales)) # to massage scales in plots
-suppressPackageStartupMessages(library(greta)) # probabilistic modeling package
-suppressPackageStartupMessages(library(ggmcmc)) # tidy Bayes samples
-suppressPackageStartupMessages(library(HDInterval)) # credible intervals
-suppressPackageStartupMessages(library(binom)) # for binomial confidence intervals 
+library(tidyverse)
+library(brms)
+library(ggsignif)
+library(gridExtra)
+library(Rlab)        # for dbern (used in appendix; maybe solve differently?)
+library(rcartocolor) # color-blind friendly palettes: https://github.com/Nowosad/rcartocolor
+library(extraDistr)  # create beta-binomial and bernoulli rvs
+library(boot)        # bootstrapped (mainly: 95% CIs for the mean)
+library(naniar)      # turn numbers (e.g. -5,99,..) into NA (handling of missing values in data sets)
+library(latex2exp)   # for TeX in plots
+library(cowplot)     # to arrange plots
+library(scales)      # to massage scales in plots
+library(greta)       # probabilistic modeling package
+library(ggmcmc)      # tidy Bayes samples
+library(HDInterval)  # credible intervals
+library(binom)       # for binomial confidence intervals 
 
 
 ## set defaults
