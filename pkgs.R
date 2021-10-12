102a8aa8a7b7820ded95354d9b575b01d2200cbf
MarauderPixie <tobias.anton.85@gmail.com>
2021-10-12 16:30:29 +0200

    bye travis, hi gh-actions (or: jumping headlong into the abyss)

--- pkgs.R
+++ pkgs.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(brms)
 library(ggsignif)
 library(gridExtra)
-library(Rlab)        # for dbern (used in appendix; maybe solve differently?)
+# library(Rlab)        # for dbern (used in appendix; maybe solve differently?)
 library(rcartocolor) # color-blind friendly palettes: https://github.com/Nowosad/rcartocolor
 library(extraDistr)  # create beta-binomial and bernoulli rvs
 library(boot)        # bootstrapped (mainly: 95% CIs for the mean)
@@ -12,28 +12,28 @@ library(latex2exp)   # for TeX in plots
 library(cowplot)     # to arrange plots
 library(scales)      # to massage scales in plots
 # library(greta)       # probabilistic modeling package
-library(ggmcmc)      # tidy Bayes samples
-library(tidybayes)   # tidy Bayes samples
-library(HDInterval)  # credible intervals
-library(binom)       # for binomial confidence intervals 
-library(BSDA)        # for z.test
-library(polspline)   # for reliable density estimates at point-values
-library(GGally)      # for ally plots 
-library(LaplacesDemon) # for student t-distribution with different means and SDs
+# library(ggmcmc)      # tidy Bayes samples
+# library(tidybayes)   # tidy Bayes samples
+# library(HDInterval)  # credible intervals
+# library(binom)       # for binomial confidence intervals 
+# library(BSDA)        # for z.test
+# library(polspline)   # for reliable density estimates at point-values
+# library(GGally)      # for ally plots 
+# library(LaplacesDemon) # for student t-distribution with different means and SDs
 library(magrittr)    # more fun with pipes
 library(zeallot)     # unpacking / multiple assignment operator %<-%
-library(mvtnorm)     # for multi-variate normal (used in lin. regression chapter)
+# library(mvtnorm)     # for multi-variate normal (used in lin. regression chapter)
 # for visualizing dirichlet distribution; 
 # needs github version, see .travis.yml for that
-if (!("dirichlet" %in% installed.packages())) {
-  devtools::install_github("dkahle/dirichlet")
-}
+# if (!("dirichlet" %in% installed.packages())) {
+#   devtools::install_github("dkahle/dirichlet")
+# }
 library(dirichlet)
-# same/similar  for aida-package:
-if (!("aida" %in% installed.packages())) {
-  devtools::install_github("michael-franke/aida-package")
-}
-library(aida)
+# # same/similar  for aida-package:
+# if (!("aida" %in% installed.packages())) {
+#   devtools::install_github("michael-franke/aida-package")
+# }
+# library(aida)
 
 ## set defaults
 # source("theme_ida.R")
